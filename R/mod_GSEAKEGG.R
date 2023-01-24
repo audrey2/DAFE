74f86ebb2d1a482ed3dcd4b573913fa9872024ea
audrey2 <audrey_beaufils@hotmail.fr>
2023-01-24 16:48:04 +0100

    Amelioration GSEA KEGG

--- R/mod_GSEAKEGG.R
+++ R/mod_GSEAKEGG.R
@@ -29,7 +29,9 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
     library(shinyFiles)
     library(png)
     library(plotly)
-
+    library(cowplot)
+    library(magick)
+    library(ggimage)
     ns <- session$ns
 
 
@@ -63,7 +65,7 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
                    DT::dataTableOutput(ns('tableau'))%>% withSpinner()
           ),tabPanel("Plot",
                      uiOutput(ns('pathIdd')),
-                     imageOutput(ns('png'))%>% withSpinner(),
+                     plotlyOutput(ns('png'))%>% withSpinner(),
                      plotlyOutput(ns('dotplotKegg'))%>% withSpinner(),
                      plotlyOutput(ns('gseaplotKegg'))%>% withSpinner(),
           ),
@@ -78,7 +80,7 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
     # Fonction chargant la librairie correspondante à l'organisme choisi
     organism<- reactive({
       organism = inputParameter$orDb
-      BiocManager::install(organism, character.only = TRUE)
+     
       library(organism, character.only = TRUE)
 
       return(organism)
@@ -88,19 +90,47 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
     # Fonction renvoyant un objet gseKEGG
     gene_list_kegg <- reactive({
       req(inputParameter$fileOr)
-      organism = organism()
-      df= read.csv(inputParameter$fileOr[1,'datapath'], header=inputParameter$header,sep=inputParameter$sep)
+      df=read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
+      
+      if(inputParameter$gsea_order=="log2FC")
+      {kegg_gene_list <- df$log2FC}
+      if(inputParameter$gsea_order=="pval")
+      {kegg_gene_list <- df$padj}
+      if(inputParameter$gsea_order=="Stat")
+      {kegg_gene_list <- df$stat}
+      df$X=df[,inputParameter$row.names]
+      names(kegg_gene_list)=df$X
 
-      original_gene_list <- df$log2FoldChange
-      names(original_gene_list) <- df$X
-      ids<-bitr(names(original_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
-      dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
-      df2 = df[df$X %in% dedup_ids$ENSEMBL,]
-      df2$Y = dedup_ids$ENTREZID
-      kegg_gene_list <- df2$log2FoldChange
-      names(kegg_gene_list) <- df2$Y
-      kegg_gene_list<-na.omit(kegg_gene_list)
-      kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
+      organism=organism()
+      ids<-bitr(names(kegg_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
+        # remove duplicate IDS (here I use "ENSEMBL", but it should be whatever was selected as keyType)
+dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
+
+# Create a new dataframe df2 which has only the genes which were successfully mapped using the bitr function above
+df2 = df[df$X %in% dedup_ids$ENSEMBL,]
+
+# Create a new column in df2 with the corresponding ENTREZ IDs
+df2$Y = dedup_ids$ENTREZID
+
+# Create a vector of the gene unuiverse
+ if(inputParameter$gsea_order=="log2FC")
+      {kegg_gene_list <- df2$log2FC}
+      if(inputParameter$gsea_order=="pval")
+      {kegg_gene_list <- df2$padj}
+      if(inputParameter$gsea_order=="Stat")
+      {kegg_gene_list <- df2$stat}
+
+
+# Name vector with ENTREZ ids
+names(kegg_gene_list) <- df2$Y
+
+# omit any NA values 
+kegg_gene_list<-na.omit(kegg_gene_list)
+
+# sort the list in decreasing order (required for clusterProfiler)
+kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
+
+
 
       kegg_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='dme','org.At.tair.db'='ath',
                                 'org.Dr.eg.db'='dre','org.Ss.eg.db'='ssc','org.Cf.eg.db'='cfa',
@@ -153,19 +183,46 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
 
   # Fonction renvoyant la liste des gènes triés par log2FC decroissant
   kge <- reactive({
-    organism = organism()
-    df= read.csv(inputParameter$fileOr[1,'datapath'], header=inputParameter$header,sep=inputParameter$sep)
+      req(inputParameter$fileOr)
+      df=read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
+      
+      if(inputParameter$gsea_order=="log2FC")
+      {kegg_gene_list <- df$log2FC}
+      if(inputParameter$gsea_order=="pval")
+      {kegg_gene_list <- df$padj}
+      if(inputParameter$gsea_order=="Stat")
+      {kegg_gene_list <- df$stat}
+      df$X=df[,inputParameter$row.names]
+      names(kegg_gene_list)=df$X
 
-    original_gene_list <- df$log2FoldChange
-    names(original_gene_list) <- df$X
-    ids<-bitr(names(original_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
-    dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
-    df2 = df[df$X %in% dedup_ids$ENSEMBL,]
-    df2$Y = dedup_ids$ENTREZID
-    kegg_gene_list <- df2$log2FoldChange
-    names(kegg_gene_list) <- df2$Y
-    kegg_gene_list<-na.omit(kegg_gene_list)
-    kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
+      organism=organism()
+      ids<-bitr(names(kegg_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
+        # remove duplicate IDS (here I use "ENSEMBL", but it should be whatever was selected as keyType)
+dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
+
+# Create a new dataframe df2 which has only the genes which were successfully mapped using the bitr function above
+df2 = df[df$X %in% dedup_ids$ENSEMBL,]
+
+# Create a new column in df2 with the corresponding ENTREZ IDs
+df2$Y = dedup_ids$ENTREZID
+
+# Create a vector of the gene unuiverse
+ if(inputParameter$gsea_order=="log2FC")
+      {kegg_gene_list <- df2$log2FC}
+      if(inputParameter$gsea_order=="pval")
+      {kegg_gene_list <- df2$padj}
+      if(inputParameter$gsea_order=="Stat")
+      {kegg_gene_list <- df2$stat}
+
+
+# Name vector with ENTREZ ids
+names(kegg_gene_list) <- df2$Y
+
+# omit any NA values 
+kegg_gene_list<-na.omit(kegg_gene_list)
+
+# sort the list in decreasing order (required for clusterProfiler)
+kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
 
     return(kegg_gene_list)})
 
@@ -179,12 +236,143 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
 
 
 # Fonction renvoyant l'image KEGG à l'ui
-   output$png<- renderImage( {list(src = pathView(),
-                                  contentType = 'image/png',
-                                  width = 400,
-                                  height = 300,
-                                  alt = "This is alternate text")
-  }, deleteFile = FALSE)
+  img=reactive({img=ggdraw() +draw_image(pathView()) 
+    return(img)})
+
+   output$png<- renderPlotly( {
+   
+
+#Constants 
+
+img_width = 1600 
+
+img_height = 900 
+
+scale_factor = 0.5 
+
+
+
+# Add invisible scatter trace. 
+
+# This trace is added to help the autoresize logic work. 
+
+fig <- plot_ly(width=img_width * scale_factor, 
+
+               height=img_height * scale_factor 
+
+) %>% 
+
+  add_trace( x= c(0, img_width * scale_factor), 
+
+             y= c(0, img_height * scale_factor), 
+
+             type = 'scatter',  mode = 'markers', alpha = 0) 
+
+
+# Configure axes 
+
+xconfig <- list( 
+
+  title = "", 
+
+  zeroline = FALSE, 
+
+  showline = FALSE, 
+
+  showticklabels = FALSE, 
+
+  showgrid = FALSE, 
+
+  range = c(0, img_width * scale_factor) 
+
+) 
+
+
+yconfig <- list( 
+
+  title = "", 
+
+  zeroline = FALSE, 
+
+  showline = FALSE, 
+
+  showticklabels = FALSE, 
+
+  showgrid = FALSE, 
+
+  range = c(0, img_height * scale_factor), 
+
+  scaleanchor="x" 
+
+) 
+
+
+fig <- fig %>% layout(xaxis = xconfig, yaxis = yconfig) 
+
+
+# Add image 
+
+
+fig <- fig %>% layout( 
+
+  images = list(  
+
+    list(  
+
+      source = raster2uri(readPNG(pathView())),  
+
+      x=0, 
+
+      sizex=img_width * scale_factor, 
+
+      y=img_height * scale_factor, 
+
+      sizey=img_height * scale_factor, 
+
+      xref="x", 
+
+      yref="y", 
+
+      opacity=1.0, 
+
+      layer="below", 
+
+      sizing="stretch" 
+
+    )  
+
+  )) 
+
+
+# Configure other layout 
+
+
+m = list(r=0, l=0, b=0, t=0) 
+
+fig <- fig %>% layout(margin = m) %>%
+
+  layout(plot_bgcolor='#e5ecf6',  
+
+          xaxis = list(  
+
+            zerolinecolor = '#ffff',  
+
+            zerolinewidth = 2,  
+
+            gridcolor = 'ffff'),  
+
+          yaxis = list(  
+
+            zerolinecolor = '#ffff',  
+
+            zerolinewidth = 2,  
+
+            gridcolor = 'ffff')  
+
+          )
+
+fig
+    })
 
   # Fonction renvoyant le dossier choisi par l'utilisateur
    pathway <- reactive({
@@ -211,7 +399,7 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
                            'org.Mmu.eg.db'='mvv','org.Xl.eg.db'='xla','org.Pf.plasmo.db'='pfa')
 
     setwd(dir)
-    pathview(gene.data=kge(), pathway.id=names(choix()[as.numeric(PATHID())]), species = kegg_organism, kegg.dir = dir,limit = list(gene=max(abs(kge())), cpd=1 ))
+    pathview(gene.data=kge(), pathway.id=names(choix()[as.numeric(PATHID())]), species = kegg_organism, kegg.dir = dir)
 
     a=paste0(dir,names(choix()[as.numeric(PATHID())]))
     print(a)
@@ -225,8 +413,8 @@ mod_GSEAKEGG_server <- function(id,inputParameter){
 
   # Fonction renvoyant le tableau de resultat de l'enrichissement
   tabb <- reactive({
-    table= cbind(gene_list_kegg()$ID,gene_list_kegg()$Description,gene_list_kegg()$enrichmentScore,gene_list_kegg()$NES,gene_list_kegg()$pvalue,gene_list_kegg()$p.adjust,gene_list_kegg()$qvalue,gene_list_kegg()$rank,gene_list_kegg()$leading_edge,gene_list_kegg()$core_enrichment)
-    colnames(table)= c('ID','Description','enrichmentScore','NES','pvalue','p.adjust','qvalues','rank','leadingEdge','coreEnrichment')
+    table= cbind(gene_list_kegg()$ID,gene_list_kegg()$Description,gene_list_kegg()$enrichmentScore,gene_list_kegg()$NES,gene_list_kegg()$pvalue,gene_list_kegg()$p.adjust,gene_list_kegg()$qvalue,gene_list_kegg()$rank)
+    colnames(table)= c('ID','Description','enrichmentScore','NES','pvalue','p.adjust','qvalues','rank')
     return(data.frame(table))
 
   })
