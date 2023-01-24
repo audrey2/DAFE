61dd153cfedc51d73f90582d21cfe71e529fe1ec
audrey2 <audrey_beaufils@hotmail.fr>
2023-01-25 00:44:03 +0100

    Module ORAKEGG amelioration

--- R/mod_ORAKEGG.R
+++ R/mod_ORAKEGG.R
@@ -54,11 +54,11 @@ mod_ORAKEGG_server <- function(id,inputParameter){
 
             ),
             column(width=3,
-                   selectInput(ns("DEG")," Choose element",choices=c('both','over','under')),
+                   sliderInput(ns("log2FC"),label="LOg2FC fold change ",min=0,max=5,value=1,step=0.1),
 
             ),
             column(width=3,
-                   shinyDirButton(ns('folder2'), 'Select a folder to save pathway plot', 'Please select a folder', FALSE),
+                   shinyDirButton(ns('folder'), 'Select a folder to save pathway plot', 'Please select a folder', FALSE),
 
             )
 
@@ -71,8 +71,8 @@ mod_ORAKEGG_server <- function(id,inputParameter){
           tabPanel("Table",
                    DT::dataTableOutput(ns('tableau'))%>% withSpinner()
           ),tabPanel("Plot",
-                     uiOutput(ns('path_Id')),
-                     imageOutput(ns('png'))%>% withSpinner(),
+                     uiOutput(ns('pathIdd')),
+                     plotlyOutput(ns('png'))%>% withSpinner(),
                      plotlyOutput(ns('dotplotK'))%>% withSpinner(),
                      plotlyOutput(ns('barplotK'))%>% withSpinner(),
           ),
@@ -83,33 +83,123 @@ mod_ORAKEGG_server <- function(id,inputParameter){
       }
       return(LL)}
     )
-    shinyDirChoose(input, 'folder2', root=c(root='~'), filetypes=c('png', 'txt'))
+    shinyDirChoose(input, 'folder', root=c(root='~'), filetypes=c('png', 'txt'))
+
+   # Fonction renvoyant le dossier choisi par l'utilisateur
+   pathway <- reactive({
+
+     a="~"
+     for( i in 1: length(input$folder)){
+
+       a= paste0(a,input$folder$path[i],"/")}
+     return(a)
+
+   })
+
+
+   # Fonction renvoyant le nom de l'image correspondant au patway KEGG
+  pathView<- reactive({
+
+    dir=pathway()
+    kegg_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='dme','org.At.tair.db'='ath',
+                           'org.Dr.eg.db'='dre','org.Ss.eg.db'='ssc','org.Cf.eg.db'='cfa',
+                           'org.Ag.eg.db'='aga','org.EcSakai.eg.db'='ecs','org.Hs.eg.db'='hsa',
+                           'org.Sc.sgd.db'='sce','org.Ce.eg.db'='sko','org.Gg.eg.db'='gga',
+                           'org.EcK12.eg.db'='eco','org.Pt.eg.db'='ptr','org.Mxanthus.db'='mxa',
+                           'org.Mm.eg.db'='mmu','org.Rn.eg.db'='rno','org.Bt.eg.db'='bta',
+                           'org.Mmu.eg.db'='mvv','org.Xl.eg.db'='xla','org.Pf.plasmo.db'='pfa')
+
+    setwd(dir)
+    pathview(gene.data=kge(), pathway.id=names(choix()[as.numeric(PATHID())]), species = kegg_organism, kegg.dir = dir)
+
+    a=paste0(dir,names(choix()[as.numeric(PATHID())]))
+    print(a)
+    a=paste0(a,".pathview.png")
+    print(a)
+
+
+    return(a)
+
+   })
+
+   choix <- reactive({
+      name=gene_list_kegg()$ID
+      num  = c(1:length(name))
+      choiceTable = data.frame(name, num)
+      choix = setNames(as.numeric(choiceTable$num), choiceTable$name)
+
+      return(choix)
+
+    })
+
+    # Fonction renvoyant à l'ui les différent pathway significativement enrichi sous la forme d'un select input
+
+  output$pathIdd <- renderUI({
+
+    L= selectInput(ns('pathiId'),"Select Path Id to observe" ,choices = choix())
+    return(L)
+
+  })
+
+
+
+    PATHID<- reactive(input$pathiId)
+
+
+
+
+
+
+
+
+
+
+
+
 
     # Fonction renvoyant un objet enrichKEGG
-    geneListK<-reactive({
+    gene_list_kegg<-reactive({
 
       req(inputParameter$fileOr)
 
-      df= read.csv(inputParameter$fileOr[1,'datapath'], header=inputParameter$header,sep=inputParameter$sep)
-      if(input$DEG=='over'){df=subset(df,log2FoldChange >0)}
-      if(input$DEG=='under'){df=subset(df,log2FoldChange <0)}
+      df=read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
+      organism=inputParameter$orDb
+      if(inputParameter$ora_order=="overexpressed"){print('over')
+      df=subset(df,log2FC >0)
+      df=subset(df,padj< input$pvalCutOff)}
+      if(inputParameter$ora_order=="underexpressed"){print('under')
+      print(head(df))
+      df=subset(df,log2FC <0)
+      print(head(df))
+      df=subset(df, padj < input$pvalCutOff)
+       print(head(df))}
+      if(inputParameter$ora_order=="both"){ df=subset(df, padj < input$pvalCutOff)}
+  
+      df$X=df[,inputParameter$row.names]
+ 
+      library(organism, character.only = TRUE)
+      original_gene_list <- df$log2FC
 
-      organism = inputParameter$orDb
-      original_gene_list <- df$log2FoldChange
-      names(original_gene_list) <- df$X
+
+
+      names(original_gene_list) <- df[,inputParameter$row.names]
+
       ids<-bitr(names(original_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
-      dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
+        # remove duplicate IDS (here I use "ENSEMBL", but it should be whatever was selected as keyType)
+        dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
+
+      # Create a new dataframe df2 which has only the genes which were successfully mapped using the bitr function above
       df2 = df[df$X %in% dedup_ids$ENSEMBL,]
+
+      # Create a new column in df2 with the corresponding ENTREZ IDs
       df2$Y = dedup_ids$ENTREZID
-      kegg_gene_list <- df2$log2FoldChange
+
+      kegg_gene_list <- df2$log2FC
+
       names(kegg_gene_list) <- df2$Y
+
       kegg_gene_list<-na.omit(kegg_gene_list)
-      kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
-      kegg_sig_genes_df = subset(df2, padj < 0.05)
-      kegg_genes <- kegg_sig_genes_df$log2FoldChange
-      names(kegg_genes) <- kegg_sig_genes_df$Y
-      kegg_genes <- na.omit(kegg_genes)
-      kegg_genes <- names(kegg_genes)[abs(kegg_genes) > 2]
+      kegg_genes <- names(kegg_gene_list)[abs(kegg_gene_list) > input$log2FC]
 
       kegg_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='dme','org.At.tair.db'='ath',
                              'org.Dr.eg.db'='dre','org.Ss.eg.db'='ssc','org.Cf.eg.db'='cfa',
@@ -120,73 +210,217 @@ mod_ORAKEGG_server <- function(id,inputParameter){
                              'org.Mmu.eg.db'='mvv','org.Xl.eg.db'='xla','org.Pf.plasmo.db'='pfa')
 
 
-
+      print(kegg_genes)
       kk= enrichKEGG(
         gene=kegg_genes,
         universe=names(kegg_gene_list),organism=kegg_organism, pvalueCutoff =input$pvalCutOff, keyType = "ncbi-geneid")
-
+      print(kk)
       return(kk)
 
     })
 
 
     kge<- reactive({
-      df= read.csv(inputParameter$fileOr[1,'datapath'], header=inputParameter$header,sep=inputParameter$sep)
-      if(input$DEG=='over'){df=subset(df,log2FoldChange >0)}
-      if(input$DEG=='under'){df=subset(df,log2FoldChange <0)}
+       req(inputParameter$fileOr)
+
+      df=read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
+      organism=inputParameter$orDb
+      if(inputParameter$ora_order=="overexpressed"){print('over')
+      df=subset(df,log2FC >0)
+      df=subset(df,padj<input$pvalCutOff)}
+      if(inputParameter$ora_order=="underexpressed"){print('under')
+      print(dim(df))
+      df=subset(df,log2FC <0)
+      df=subset(df,padj<input$pvalCutOff)
+      print(dim(df))}
+  if(inputParameter$ora_order=="both"){ df=subset(df, padj < input$pvalCutOff)}
+  
+      df$X=df[,inputParameter$row.names]
+      
+   
+      library(organism, character.only = TRUE)
+      original_gene_list <- df$log2FC
 
 
-      original_gene_list <- df$log2FoldChange
-      names(original_gene_list) <- df$X
-      gene_list<-na.omit(original_gene_list)
-      gene_list = sort(gene_list, decreasing = TRUE)
+
+      names(original_gene_list) <- df[,inputParameter$row.names]
+
+      ids<-bitr(names(original_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
+        # remove duplicate IDS (here I use "ENSEMBL", but it should be whatever was selected as keyType)
+      dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
+
+      # Create a new dataframe df2 which has only the genes which were successfully mapped using the bitr function above
+      df2 = df[df$X %in% dedup_ids$ENSEMBL,]
+
+      # Create a new column in df2 with the corresponding ENTREZ IDs
+      df2$Y = dedup_ids$ENTREZID
+
+      kegg_gene_list <- df2$log2FC
+
+      names(kegg_gene_list) <- df2$Y
+
+      kegg_gene_list<-na.omit(kegg_gene_list)
+
+      
+
+      
+  
+
+
+      kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
+      duplicated_names <- duplicated(names(kegg_gene_list))
+
+
+      kegg_gene_list=kegg_gene_list[!duplicated_names]
+
+      return(kegg_gene_list)
+
+
     })
+
     # Fonction renvoyant l'image KEGG à l'ui
-    output$png<- renderImage( {list(src = pathView(),
-                                    contentType = 'image/png',
-                                    width = 400,
-                                    height = 300,
-                                    alt = "This is alternate text")
-    }, deleteFile = FALSE)
+    output$png<- renderPlotly( {
+   
 
-    # Fonction renvoyant le dossier choisi par l'utilisateur
-    pathway <- reactive({
+      #Constants 
 
-      a="~"
-      for( i in 1: length(input$folder2)){
+    img_width = 1600 
 
-        a= paste0(a,input$folder2$path[i],"/")}
-      return(a)
+    img_height = 900 
 
+    scale_factor = 0.5 
+
+
+
+
+
+fig <- plot_ly(width=img_width * scale_factor, 
+
+               height=img_height * scale_factor 
+
+) %>% 
+
+  add_trace( x= c(0, img_width * scale_factor), 
+
+             y= c(0, img_height * scale_factor), 
+
+             type = 'scatter',  mode = 'markers', alpha = 0) 
+
+
+# Configure axes 
+
+xconfig <- list( 
+
+  title = "", 
+
+  zeroline = FALSE, 
+
+  showline = FALSE, 
+
+  showticklabels = FALSE, 
+
+  showgrid = FALSE, 
+
+  range = c(0, img_width * scale_factor) 
+
+) 
+
+
+yconfig <- list( 
+
+  title = "", 
+
+  zeroline = FALSE, 
+
+  showline = FALSE, 
+
+  showticklabels = FALSE, 
+
+  showgrid = FALSE, 
+
+  range = c(0, img_height * scale_factor), 
+
+  scaleanchor="x" 
+
+) 
+
+
+fig <- fig %>% layout(xaxis = xconfig, yaxis = yconfig) 
+
+
+# Add image 
+
+
+fig <- fig %>% layout( 
+
+  images = list(  
+
+    list(  
+
+      source = raster2uri(readPNG(pathView())),  
+
+      x=0, 
+
+      sizex=img_width * scale_factor, 
+
+      y=img_height * scale_factor, 
+
+      sizey=img_height * scale_factor, 
+
+      xref="x", 
+
+      yref="y", 
+
+      opacity=1.0, 
+
+      layer="below", 
+
+      sizing="stretch" 
+
+    )  
+
+  )) 
+
+
+# Configure other layout 
+
+
+m = list(r=0, l=0, b=0, t=0) 
+
+fig <- fig %>% layout(margin = m) %>%
+
+  layout(plot_bgcolor='#e5ecf6',  
+
+          xaxis = list(  
+
+            zerolinecolor = '#ffff',  
+
+            zerolinewidth = 2,  
+
+            gridcolor = 'ffff'),  
+
+          yaxis = list(  
+
+            zerolinecolor = '#ffff',  
+
+            zerolinewidth = 2,  
+
+            gridcolor = 'ffff')  
+
+          )
+
+fig
     })
-    # Fonction renvoyant le nom de l'image correspondant au patway KEGG
-    pathView<- reactive({
+
+   
 
 
-      dir=pathway()
-      kegg_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='dme','org.At.tair.db'='ath',
-                             'org.Dr.eg.db'='dre','org.Ss.eg.db'='ssc','org.Cf.eg.db'='cfa',
-                             'org.Ag.eg.db'='aga','org.EcSakai.eg.db'='ecs','org.Hs.eg.db'='hsa',
-                             'org.Sc.sgd.db'='sce','org.Ce.eg.db'='sko','org.Gg.eg.db'='gga',
-                             'org.EcK12.eg.db'='eco','org.Pt.eg.db'='ptr','org.Mxanthus.db'='mxa',
-                             'org.Mm.eg.db'='mmu','org.Rn.eg.db'='rno','org.Bt.eg.db'='bta',
-                             'org.Mmu.eg.db'='mvv','org.Xl.eg.db'='xla','org.Pf.plasmo.db'='pfa')
-
-
-      d=pathview(gene.data=kge(), pathway.id=names(choix()[as.numeric(PATHID())]), species = kegg_organism, kegg.dir = dir )
-
-      a=paste0(dir,names(choix()[as.numeric(PATHID())]))
-      a=paste0(a,".pathview.png")
-
-      return(a)
-
-    })
-
+   
 
     # Fonctions renvoyant les plot
     output$dotplotK <- renderPlotly({
 
-      gbb=geneListK()
+      gbb=gene_list_kegg()
       require(DOSE)
       ggplotly(dotplot(gbb, showCategory=10))
 
@@ -194,7 +428,7 @@ mod_ORAKEGG_server <- function(id,inputParameter){
 
     output$barplotK <- renderPlotly({
 
-      gbb=geneListK()
+      gbb=gene_list_kegg()
 
 
       ggplotly(barplot(gbb,
@@ -212,40 +446,12 @@ mod_ORAKEGG_server <- function(id,inputParameter){
 
 
     # Fonction renvoyant à l'ui les différent pathway significativement enrichi sous la forme d'un select input
-    output$path_Id <- renderUI({
-
-      L= selectInput(ns('ID_path'),"Select Path Id to observe" ,choices = choix())
-      return(L)
-
-    })
-    PATHID<- reactive(input$ID_path)
-
-    # Fonction renvoyant les différent pathway significativement enrichi
-  choix <- reactive({
-
-    name=geneListK()$ID
-
-    num  = c(1:length(name))
-
-
-    choiceTable = data.frame(name, num)
-    choix = setNames(as.numeric(choiceTable$num), choiceTable$name)
-    return(choix)
-
-  })
-  # Fonction renvoyant l'image KEGG à l'ui
-  output$png<- renderImage( {list(src = pathView(),
-                                  contentType = 'image/png',
-                                  width = 400,
-                                  height = 300,
-                                  alt = "This is alternate text")
-  }, deleteFile = FALSE)
-
+     
   # Fonction renvoyant le tableau de resultat de l'enrichissement
   tabb <- reactive({
+     
 
-
-    table=cbind(geneListK()$ID,geneListK()$Description,geneListK()$GeneRatio,geneListK()$BgRatio,geneListK()$pvalue,geneListK()$p.adjust,geneListK()$qvalue,geneListK()$geneID)
+    table=cbind(gene_list_kegg()$ID,gene_list_kegg()$Description,gene_list_kegg()$GeneRatio,gene_list_kegg()$BgRatio,gene_list_kegg()$pvalue,gene_list_kegg()$p.adjust,gene_list_kegg()$qvalue,gene_list_kegg()$geneID)
     colnames(table)=cbind('ID','Description','GeneRatio','BgRatio','pvalue','p.adjust','qvalue','geneID')
     return( data.frame(table))
   })
