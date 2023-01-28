bc17b99bb28224690ed8c374260f13ea0e0247c4
audrey2 <audrey_beaufils@hotmail.fr>
2023-01-28 18:14:03 +0100

    Restructuration module ORAReactome

--- R/mod_ORAReactome.R
+++ R/mod_ORAReactome.R
@@ -23,191 +23,185 @@ mod_ORAReactome_server <- function(id,inputParameter){
     ns <- session$ns
 
 
+# UI OUTPUT
     #Fonction renvoyant l'ui si la méthode choisi est ORA
     output$condPanel<- renderUI({
       LL=c()
 
       if(inputParameter$Gsea_ORA == 'ORA'){
-
-        LL[[1]] = fluidRow(box(width=13,class='VIOLET',
-
-          h1('ORA Reactome settings',icon('cogs')),
-          hr(),
+        LL[[1]] = fluidRow(box(width = 12,status = 'info',solidHeader = TRUE,collapsible=TRUE,title=h1('ORA Reactome settings',icon('gear')),
           fluidRow(
-            column(width=2),
+            column(width=1),
             column(width=3,
-
-              sliderInput(ns("pvalCutOff"),label = "p-Value cutoff from input ",
+              sliderInput(ns("pvalI"),label = "p-Value cutoff from input ",
                           min = 0, max =0.1, value = 0.05,step=0.01),
-
             ),
-            column(width=3,sliderInput(ns("log2FC"),label="LOg2FC fold change ",min=0,max=5,value=1,step=0.1)
-                   
-
+            column(width=3,sliderInput(ns("log2I"),label = "Log2 Fold change cutoff from input ",
+                          min = 0, max =5, value = 2,step=0.5)
             ),
-            column(width=3
-                   
-            )
+            column(width=3, sliderInput(ns("pvalCutOff"),label = "p-Value cutoff from output ",
+                          min = 0, max =0.1, value = 0.05,step=0.01)
+            ),
+            column(width=2,br(),br(),br(),actionButton(ns("go"),"Start",class="buttS",icon("play")))
           )
-
         ))
 
-        LL[[2]] = br()
-        LL[[3]] = fluidRow(tabsetPanel(
-          tabPanel("Table",
-                   DT::dataTableOutput(ns('tableau'))%>% withSpinner()
-          ),tabPanel("Plot",
-
-
-                     plotlyOutput(ns('dotplotR'))%>% withSpinner(),
-                     plotlyOutput(ns('barplotR'))%>% withSpinner(),
-                     plotlyOutput(ns('pathwayR'))%>% withSpinner()
-          ),
+        LL[[2]] = fluidRow( tabBox(width=12,height=NULL,
+          tabPanel("Table",fluidRow(
+            column(width=12,
+              box(width = NULL,status = 'primary',solidHeader = TRUE,title=h1('Table of results',icon('table')),
+                DT::dataTableOutput(ns('tableau'))%>% withSpinner()
+              )
+            )
+          )),
+          tabPanel("Plot",fluidRow(
+            column(width=12,
+              box(width = NULL,status = 'success',solidHeader = TRUE,title=h1('Reactome pathway',icon('chart-simple')),
+                  uiOutput(ns('pathIdd')),
+                  uiOutput(ns('titlePath')),
+                 plotOutput(ns('pathwayR'))%>% withSpinner()
+            ),
+            column(width=6,
+              box(width = NULL,status = 'success',solidHeader = TRUE,title=h1('Dotplot',icon('chart-simple')),
+                numericInput(ns("numberCat"), min=1,value=10,label="Write number of category to show",width="20%"),
+                plotlyOutput(ns('dotplotR'))%>% withSpinner()
+              )
+            ),
+            column(width=6,
+              box(width = NULL,status = 'success',solidHeader = TRUE,title=h1('Barplot',icon('chart-simple')),
+                numericInput(ns("numberCat2"), min=1,value=10,label="Write number of category to show",width="20%"),
+                plotlyOutput(ns('barplotR'))%>% withSpinner()
+              )
+            )
+            
+            )
+          ))
         ))
-
       }
-      return(LL)}
-    )
+      return(LL)
+    })
 
 
     shinyDirChoose(input, 'folder2', root=c(root='~'), filetypes=c('png', 'txt'))
 
+    # Fonction renvoyant à l'ui les différent pathway significativement enrichi sous la forme d'un select input pour le pathway KEGG
+    output$pathIdd <- renderUI({
 
+      selectInput(ns('pathiId'),"Select Path Id to observe" ,choices = choix(),width="20%")
+    })
+    PATHID<- reactive(input$pathiId)
+
+  # Fonction renvoyant le nom du pathway reactome observe   
+  output$titlePath <- renderUI({
+      num_path=as.numeric( PATHID())
+      h4(geneListR()$Description[num_path])
+      })
+
+
+# FUNCTION 
     # Fonction renvoyant un objet enrichpathway
-    geneListR<-reactive({
-  req(inputParameter$fileOr)
+    geneListR<-eventReactive(input$go,{
+      
+      kegg_gene_list=kge()
+      kegg_genes = names(kegg_gene_list)[abs(kegg_gene_list) > input$log2I]
+
+      react_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='fly',
+                             'org.Dr.eg.db'='zebrafish','org.Hs.eg.db'='human',
+                             'org.Sc.sgd.db'='yeast','org.Ce.eg.db'='celegans',
+                             'org.Mm.eg.db'='mouse','org.Rn.eg.db'='rat')
+
+      oraPath= enrichPathway(
+        gene=kegg_genes,pvalueCutoff = input$pvalCutOff, readable=TRUE,organism=react_organism
+      )
+      return(oraPath)
+
+    })
+
+
+   kge<- eventReactive(input$go,{
+      req(inputParameter$fileOr)
 
       df=read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
       organism=inputParameter$orDb
-      if(inputParameter$ora_order=="overexpressed"){print('over')
-      df=subset(df,log2FC >0)
-      df=subset(df,padj< input$pvalCutOff)}
-      if(inputParameter$ora_order=="underexpressed"){print('under')
-      print(head(df))
-      df=subset(df,log2FC <0)
-      print(head(df))
-      df=subset(df, padj < input$pvalCutOff)
-       print(head(df))}
+      if(inputParameter$ora_order=="overexpressed"){
+        df=subset(df,log2FC >0)
+        df=subset(df,padj< input$pvalCutOff)
+      }
+      if(inputParameter$ora_order=="underexpressed"){
+        df=subset(df,log2FC <0)
+        df=subset(df, padj < inputParameter$pvalI)
+      }
       if(inputParameter$ora_order=="both"){ df=subset(df, padj < input$pvalCutOff)}
   
       df$X=df[,inputParameter$row.names]
  
       library(organism, character.only = TRUE)
-      original_gene_list <- df$log2FC
+      original_gene_list = df$log2FC
 
+      names(original_gene_list) = df[,inputParameter$row.names]
 
-
-      names(original_gene_list) <- df[,inputParameter$row.names]
-
-      ids<-bitr(names(original_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
-        # remove duplicate IDS (here I use "ENSEMBL", but it should be whatever was selected as keyType)
-        dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
-
-      # Create a new dataframe df2 which has only the genes which were successfully mapped using the bitr function above
-      df2 = df[df$X %in% dedup_ids$ENSEMBL,]
-
-      # Create a new column in df2 with the corresponding ENTREZ IDs
-      df2$Y = dedup_ids$ENTREZID
-
-      kegg_gene_list <- df2$log2FC
-
-      names(kegg_gene_list) <- df2$Y
-
-      kegg_gene_list<-na.omit(kegg_gene_list)
-      kegg_genes <- names(kegg_gene_list)[abs(kegg_gene_list) > input$log2FC]
-
-react_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='fly',
-                                'org.Dr.eg.db'='zebrafish','org.Hs.eg.db'='human',
-                             'org.Sc.sgd.db'='yeast','org.Ce.eg.db'='celegans',
-                             'org.Mm.eg.db'='mouse','org.Rn.eg.db'='rat')
-
-    
-      kk= enrichPathway(
-        gene=kegg_genes,pvalueCutoff = input$pvalCutOff, readable=TRUE,organism=react_organism
-
-       )
-
-      return(kk)
-
-    })
-
-
-   kge<- reactive({
-       req(inputParameter$fileOr)
-
-      df=read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
-      organism=inputParameter$orDb
-      if(inputParameter$ora_order=="overexpressed"){print('over')
-      df=subset(df,log2FC >0)
-      df=subset(df,padj< input$pvalCutOff)}
-      if(inputParameter$ora_order=="underexpressed"){print('under')
-      print(head(df))
-      df=subset(df,log2FC <0)
-      print(head(df))
-      df=subset(df, padj < input$pvalCutOff)
-       print(head(df))}
-      if(inputParameter$ora_order=="both"){ df=subset(df, padj < input$pvalCutOff)}
-  
-      df$X=df[,inputParameter$row.names]
-      
-   
-      library(organism, character.only = TRUE)
-      original_gene_list <- df$log2FC
-
-
-
-      names(original_gene_list) <- df[,inputParameter$row.names]
-
-      ids<-bitr(names(original_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
-        # remove duplicate IDS (here I use "ENSEMBL", but it should be whatever was selected as keyType)
+      ids=bitr(names(original_gene_list), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=organism)
       dedup_ids = ids[!duplicated(ids[c("ENSEMBL")]),]
 
-      # Create a new dataframe df2 which has only the genes which were successfully mapped using the bitr function above
       df2 = df[df$X %in% dedup_ids$ENSEMBL,]
-
-      # Create a new column in df2 with the corresponding ENTREZ IDs
       df2$Y = dedup_ids$ENTREZID
 
-      kegg_gene_list <- df2$log2FC
+      kegg_gene_list = df2$log2FC
 
-      names(kegg_gene_list) <- df2$Y
+      names(kegg_gene_list) = df2$Y
 
-      kegg_gene_list<-na.omit(kegg_gene_list)
-
-      
-
-      
-  
-
-
-      kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
-      duplicated_names <- duplicated(names(kegg_gene_list))
-
-
-      kegg_gene_list=kegg_gene_list[!duplicated_names]
+      kegg_gene_list=na.omit(kegg_gene_list)
 
       return(kegg_gene_list)
 
 
     })
+   # Fonction renvoyant les différent GO terms significativement enrichi
+    choix <- reactive({
 
-# Fonctions renvoyant les graphiques à l'ui
-    output$dotplotR <- renderPlotly({
+      name=geneListR()$ID
+      num  = c(1:length(name))
+      choiceTable = data.frame(name, num)
+      choix = setNames(as.numeric(choiceTable$num), choiceTable$name)
 
-      gbb=geneListR()
-      require(DOSE)
-      ggplotly(dotplot(gbb, showCategory=10))
+      return(choix)
 
     })
 
+# OUTPUT
+## TABLE
+  # Fonction renvoyant le tableau de resultat de l'enrichissement
+    tabb <- reactive({
+      table=cbind(geneListR()$ID,geneListR()$Description,geneListR()$GeneRatio,geneListR()$BgRatio,geneListR()$pvalue,geneListR()$p.adjust,geneListR()$qvalue,geneListR()$geneID)
+      colnames(table)=cbind('ID','Description','GeneRatio','BgRatio','pvalue','p.adjust','qvalue','geneID')
+      return( data.frame(table))
+    })
+
+    # Fonction renvoyant à l'ui la datatable des resultats
+    output$tableau <- DT::renderDataTable(
+      DT::datatable({
+       tabb()
+      },
+      extensions = 'Buttons',
+      caption="Table: ORA table of Reactome Pathway",
+      options = list(
+        dom = 'Bfrtip',
+        buttons = c('copy', 'csv', 'excel')
+      ), class = "display"
+    ))
+## PLOT  
+  # Fonctions renvoyant le dot plot
+    output$dotplotR <- renderPlotly({
+
+      require(DOSE)
+      ggplotly(dotplot(geneListR(), showCategory=input$numberCat))
+
+    })
+    # Fonctions renvoyant le barplot
     output$barplotR <- renderPlotly({
+      ggplotly(barplot(geneListR(),
 
-      gbb=geneListR()
-
-
-      ggplotly(barplot(gbb,
-
-                       showCategory = 10,
+                       showCategory = input$numberCat2,
                        title = "Barplot Recatome Pathways",
                        font.size = 8))
 
@@ -215,9 +209,7 @@ react_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='fly',
 
 
     # Fonction renvoyant le nom de l'image correspondant au pathway Reactome
-    output$pathwayR <- renderPlotly({
-
-      gbb=geneListR()
+    output$pathwayR <- renderPlot({
 
       kg=switch(inputParameter$orDb,'org.Dm.eg.db'='dme','org.At.tair.db'='ath',
                 'org.Dr.eg.db'='dre','org.Ss.eg.db'='ssc','org.Cf.eg.db'='cfa',
@@ -227,50 +219,15 @@ react_organism = switch(inputParameter$orDb,'org.Dm.eg.db'='fly',
                 'org.Mm.eg.db'='mmu','org.Rn.eg.db'='rno','org.Bt.eg.db'='bta',
                 'org.Mmu.eg.db'='mvv','org.Xl.eg.db'='xla','org.Pf.plasmo.db'='pfa')
       react=switch(kg,'dme'='fly' ,'ath'='arabidopsis','dre'='zebrafish','ssc'='pig','cfa'='canine','ecs'='ecolik12','hsa'='human','sce'='yeast','sko'='celegans','gga'='chicken','eco'='ecolik12','mmu'='mouse','rno'='rat','bta'='bovine','xla'='xenopus')
-
-      ggplotly(viewPathway(gbb$Description[1],
+      num_path=as.numeric( PATHID())
+      
+     viewPathway(geneListR()$Description[num_path+1],
                            readable = TRUE,
                            organism=react,
-                           foldChange = kge()))
+                           foldChange = kge())
+      
+      
 
-    })
-
-
-
-
-
-    # Fonction renvoyant le tableau de resultat de l'enrichissement
-    tabb <- reactive({
-
-
-      table=cbind(geneListR()$ID,geneListR()$Description,geneListR()$GeneRatio,geneListR()$BgRatio,geneListR()$pvalue,geneListR()$p.adjust,geneListR()$qvalue,geneListR()$geneID)
-      colnames(table)=cbind('ID','Description','GeneRatio','BgRatio','pvalue','p.adjust','qvalue','geneID')
-      return( data.frame(table))
-    })
-
-    # Fonction renvoyant à l'ui la datatable des resultats
-    output$tableau <- DT::renderDataTable(DT::datatable(
-      { tabb()},
-
-      extensions = 'Buttons',
-      caption="Table: ORA table of Reactome Pathway",
-      options = list(
-
-        dom = 'Bfrtip',
-        buttons = c('copy', 'csv', 'excel')
-      ),
-
-      class = "display"
-    ))
-
-
-
-  }
-  )
+    })    
+  })
 }
-
-## To be copied in the UI
-# mod_ORAReactome_ui("ORAReactome_1")
-
-## To be copied in the server
-# mod_ORAReactome_server("ORAReactome_1")
