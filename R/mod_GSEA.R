9589d328e28943b8d4714a34c3c96ac588262692
audrey2 <audrey_beaufils@hotmail.fr>
2023-01-28 19:13:22 +0100

    Restructuration module GSEA

--- R/mod_GSEA.R
+++ R/mod_GSEA.R
@@ -1,6 +1,6 @@
 #' GSEA UI Function
 #'
-#' @description A shiny Module.
+#' @description This module run Gen Set Enrichment Analysis on GO terms.
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
@@ -10,19 +10,8 @@
 mod_GSEA_ui <- function(id){
   ns <- NS(id)
   tagList(
-
-
-  hr(),
-  uiOutput(ns('condPanel'))
-
-
-
-
-
-
-
-    )
-
+    uiOutput(ns('condPanel'))
+  )
 }
 
 #' GSEA Server Functions
@@ -41,7 +30,6 @@ mod_GSEA_server <- function(id,inputParameter){
 
     ns <- session$ns
 
-
     #Fonction renvoyant l'ui si la méthode choisi est GSEA
     output$condPanel<- renderUI({
       LL=c()
@@ -49,77 +37,81 @@ mod_GSEA_server <- function(id,inputParameter){
 
 
         LL[[1]]=fluidRow(box(width = 12,status = 'info',solidHeader = TRUE,collapsible=TRUE,title=h1('GSEA GO terms settings',icon('gear')),
-          hr(),
+      
           fluidRow(
+            column(width=1),
             column(width=3,
-          selectInput(ns("onto"),"Choose GO annotation", choices=c("Biological Process"='BP', "Cellular Component"="CC","Molecular Function"="MF","ALL"="ALL" ))),
-          column(width=3,
-          sliderInput(ns("pvalCutOff"),label = "p-Value cutoff from input ",
-                      min = 0, max =0.1, value = 0.05,step=0.01)),
-          column(width=3,
-          sliderInput(ns("GoLevel"),label="Select Go Level",min=1,max=7,value=4,step=1)),
-        )
+              selectInput(ns("onto"),"Choose GO annotation", choices=c("Biological Process"='BP', "Cellular Component"="CC","Molecular Function"="MF","ALL"="ALL" ))),
+            column(width=3,
+              sliderInput(ns("pvalCutOff"),label = "p-Value cutoff from output ",
+                      min = 0, max =0.1, value = 0.05,step=0.01)
+            ),
+            column(width=3,
+              sliderInput(ns("GoLevel"),label="Select Go Level",min=1,max=7,value=4,step=1)
+            ),
+            column(width=2,br(),br(),br()
+              ,actionButton(ns("go"),"Start",class="buttS",icon("play")))
+          )
         ))
 
-
-      LL[[3]]=fluidRow(tabBox(width=12,
-        tabPanel("Table",
-                 box(width = 12,status = 'primary',solidHeader = TRUE,title=h1('Table of results',icon('table')),
-                 DT::dataTableOutput(ns('tableau'))%>% withSpinner())
-        ),tabPanel("Plot", box(width = 6,status = 'success',solidHeader = TRUE,title=h1('Dotplot',icon('char-simple')),
-                     plotlyOutput(ns('dotplot'))%>% withSpinner()),
-
-                   box(width = 6,status = 'success',solidHeader = TRUE,title=h1('Gseaplot',icon('char-simple')),
-
-                   uiOutput(ns('pathId')),plotOutput(ns('gseaplot'))%>% withSpinner()
-        )),
+        LL[[2]]=fluidRow(tabBox(width=12,height=NULL,
+          tabPanel("Table",
+            fluidRow(
+              column(width=12,
+                box(width = NULL,status = 'primary',solidHeader = TRUE,title=h1('Table of results',icon('table')),
+                  DT::dataTableOutput(ns('tableau'))%>% withSpinner()
+                )
+              )
+            )
+          ),
+          tabPanel("Plot",
+            fluidRow(
+              column(width=6,
+                box(width = NULL,status = 'success',solidHeader = TRUE,title=h1('Dotplot',icon('chart-simple')),
+                  uiOutput(ns('numberCategory')),
+                  plotlyOutput(ns('dotplot'))%>% withSpinner()
+                )
+              ),
+              column(width=6,
+                box(width = NULL,status = 'success',solidHeader = TRUE,title=h1('GSEAplot',icon('chart-simple')),
+                  uiOutput(ns('pathId')),
+                  plotOutput(ns('gseaplot'))%>% withSpinner()
+                )
+              )
+            )
+          )
         ))
+      }
+      return(LL)  
+    })
+    # Fonction renvoyant à l'ui un slideerInput pour deteminer le nombre de categrories a afficher sur le dotPlot
+    output$numberCategory<-renderUI({
+        numericInput(ns("numberCat"), min=1,value=min(5,nrow(tabb())),max=nrow(tabb()),label="Write number of category to show",width="20%")
+    })
 
+    # Fonction renvoyant à l'ui les différent GO terms significativement enrichi sous la forme d'un select input
+    output$pathId <- renderUI({
 
+      L= selectInput(ns('pathId'),"Select Pathway Id to observe" ,choices = choix(),width="20%")
+      return(L)
 
-}
-      return(LL)}
-    )
+    })
 
-    # Fonction chargant la librairie correspondante à l'organisme choisi
-    organism<- reactive({
+# FUNCTION
+
+    # Fonction renvoyant un objet GSEA enfonction des spécificité choisi
+    geneList<-eventReactive(input$go,{
+      req(inputParameter$fileOr)
+      df = read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
       organism = inputParameter$orDb
      
       library(organism, character.only = TRUE)
 
-      return(organism)
-    })
 
+      if(inputParameter$gsea_order=="log2FC") {original_gene_list <- df$log2FC}
+      if(inputParameter$gsea_order=="pval")   {original_gene_list <- df$padj}
+      if(inputParameter$gsea_order=="Stat")   {original_gene_list <- df$stat}     
 
-
-    # Fonction renvoyant le tableau charger
-    df <-reactive({
-
-      req(inputParameter$fileOr)
-
-
-      df = read.csv(inputParameter$fileOr[1,'datapath'], header=TRUE,sep=inputParameter$sep)
-
-      return(df)
-
-    })
-
-
-    # Fonction renvoyant un objet GSEA enfonction des spécificité choisi
-    geneList<-function(){
-      df=df()
-
-      organism=organism()
-
-
-      if(inputParameter$gsea_order=="log2FC")
-      {original_gene_list <- df$log2FC}
-      if(inputParameter$gsea_order=="pval")
-      {original_gene_list <- df$padj}
-      if(inputParameter$gsea_order=="Stat")
-      {original_gene_list <- df$stat}
-      print(inputParameter$row.names)
-      print(df[,inputParameter$row.names])
       row.names(df)=df[,inputParameter$row.names]
       names(original_gene_list) <- row.names(df)
       gene_list<-na.omit(original_gene_list)
@@ -131,25 +123,15 @@ mod_GSEA_server <- function(id,inputParameter){
                    minGSSize = 3,
                    maxGSSize = 800,
                    pvalueCutoff =input$pvalCutOff,
-                   verbose = TRUE,
+                   verbose = FALSE,
                    OrgDb = organism,
                    pAdjustMethod = inputParameter$pAdjMethod)
 
-print('aaaaaaaaaaaaa')
-      #gse=simplify(
-       # gse,
-        #cutoff = 0.7,
-       # by = "p.adjust",
-        #select_fun = min,
-        #measure = "Wang",
-        #semData = NULL
-      #)
-print('bbbbbbbbbbbbbbbbb')
       gse=goFilter(gse,input$GoLevel)
-    print('ccccccccccccccccccccc')
+
       return(gse)
 
-    }
+    })
 
     # Fonction renvoyant les différent GO terms significativement enrichi
     choix <- reactive({
@@ -161,46 +143,16 @@ print('bbbbbbbbbbbbbbbbb')
 
     })
 
-    # Fonction renvoyant à l'ui les différent GO terms significativement enrichi sous la forme d'un select input
-    output$pathId <- renderUI({
-
-      L= selectInput(ns('pathId'),"Select Path Id to observe" ,choices = choix())
-      return(L)
-
-    })
-
-
-
-    gg <- reactive({
-
-      return(geneList())
-    })
-    # Fonction renvoyant un dotplot
-    output$dotplot <- renderPlotly({
-
-      withProgress(message = "Plotting dotPlot ...",{
-        gbb=gg()
-        require(DOSE)
-        ggplotly(dotplot(gbb, showCategory=10, split=".sign") + facet_grid(.~.sign))})
-
-    })
-
-    # Fonction renvoyant le GSEA plot
-    output$gseaplot <- renderPlot({
-
-      gbb=geneList()
-      require(DOSE)
-      enrichplot::gseaplot2(gbb, title = gbb$Description[as.numeric(input$pathId)], geneSetID = as.numeric(input$pathId))
-
-    })
-
-    # Fonction renvoyant le tableau d'enrcihissement
+    
+# OUTPUT
+## TABLE
+# Fonction renvoyant le tableau d'enrcihissement
     tabb <- reactive({
-      table=gg()
-      print(head(table))
+      table=geneList()
+     
       table= cbind(table$ID,table$Description,table$enrichmentScore,table$NES,table$pvalue,table$p.adjust,table$qvalue,table$rank)
       colnames(table)= c('ID','Description','enrichmentScore','NES','pvalue','p.adjust','qvalues','rank')
-  return(data.frame(table))
+      return(data.frame(table))
 
     })
 
@@ -219,17 +171,25 @@ print('bbbbbbbbbbbbbbbbb')
       class = "display"
     ))
 
+## PLOT
+    # Fonction renvoyant un dotplot
+    output$dotplot <- renderPlotly({
+
+      withProgress(message = "Plotting dotPlot ...",{
+  
+        require(DOSE)
+        ggplotly(dotplot(geneList(), showCategory=input$numberCat, split=".sign") + facet_grid(.~.sign))})
+
+    })
+
+    # Fonction renvoyant le GSEA plot
+    output$gseaplot <- renderPlot({
 
 
+      require(DOSE)
+      enrichplot::gseaplot2(geneList(), title = geneList()$Description[as.numeric(input$pathId)], geneSetID = as.numeric(input$pathId))
 
-
-  return(input)
+    })
 
   })
 }
-
-## To be copied in the UI
-# mod_GSEA_ui("GSEA_1")
-
-## To be copied in the server
-# mod_GSEA_server("GSEA_1")
